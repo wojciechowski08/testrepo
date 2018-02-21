015b7804779d649001e4b9bfefd0d85948c193c6
Daniel Wojciechowski <wojciechowski.daniel.1998@gmail.com>
2018-02-21 01:24:54 +0100

    adding compareTo

--- src/Dog.java
+++ src/Dog.java
@@ -1,17 +1,25 @@
-public class Dog {
+public class Dog implements Comparable<Dog> {
 
+    private double weight;
     private String name;
     private String breed;
     private int age;
 
-    public Dog(String name, String breed, int age) {
+    public Dog(String name, String breed, int age, double weight) {
         this.name = name;
         this.breed = breed;
         this.age = age;
+        this.weight = weight;
     }
 
     public void bark() {
         System.out.print("bark");
     }
 
+    @Override
+    public int compareTo(Dog o) {
+        if (this.weight > o.weight)         { return 1; }
+        else if (this.weight < o.weight)    { return -1; }
+        else                                { return 0; }
+    }
 }
